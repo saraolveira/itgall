0acbde71015ba8178f0edc0e0e10c14efda95410
Sara Olveira <saralouro7@gmail.com>
2024-02-22 11:52:39 +0100

    fix image

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 const BlogPost = ({ title, slug, excerpt, img }) => {
     return (
         <Link to={`/posts/${slug}`}>
-            <div className="w-full lg:w-96 h-[32rem] bg-white p-8 rounded-3xl drop-shadow-md hover:cursor-pointer group">
+            <div className="w-full lg:w-96 h-[32rem] bg-white p-8 rounded-3xl drop-shadow-md hover:cursor-pointer group relative">
                 <img
                     src={img ? img : undefined}
                     className="object-cover	w-full lg:w-96 h-40 rounded-t-3xl absolute top-0 left-0 group-hover:grayscale transition duration-300"
